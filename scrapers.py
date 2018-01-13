7150ae03665483e31ff48e38b1d4aa190e73bfe2
cyian-1756 <devnull64@vfemail.net>
2018-01-12 19:09:47 -0500

    Added helper func, sorted imports

--- scrapers.py
+++ scrapers.py
@@ -1,9 +1,9 @@
-import requests
 import json
-from bs4 import BeautifulSoup
-import csv
 import os
 import re
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def load_json():
@@ -24,6 +24,12 @@ def get_game_download_title(r):
     except KeyError:
         return None
 
+def write_json_game_db(json_list):
+    with open('temp_json', 'w', encoding="utf-8") as f:
+         f.write(json.dumps(json_list, indent=4, sort_keys=True))
+    os.remove("games.json")
+    os.rename("temp_json", "games.json")
+
 # Download links for insexsity are just http://insexsity.com/$VERSION_$OS_64_(U).zip
 # so they can be easily generated.
 # Note: insexsity calls the OS "windows" "PC"
@@ -34,12 +40,8 @@ def update_insexsity_download_links(old_version, new_version):
             for opsys in ["windows", "mac", "android"]:
                 sub_array["download_link_{}".format(opsys)] = sub_array["download_link_{}".format(opsys)].replace(old_version, new_version)
         json_list.append(sub_array)
-    with open('temp_json', 'w', encoding="utf-8") as f:
-         f.write(json.dumps(json_list, indent=4))
-    os.remove("games.json")
-    os.rename("temp_json", "games.json")
-
-
+    write_json_game_db(json_list)
+    
 
 def update_json_version(game_name, new_version):
     json_list = []
@@ -47,10 +49,7 @@ def update_json_version(game_name, new_version):
         if sub_array["game"].lower() == game_name.lower():
             sub_array["latest_version"] = new_version
         json_list.append(sub_array)
-    with open('temp_json', 'w', encoding="utf-8") as f:
-         f.write(json.dumps(json_list, indent=4))
-    os.remove("games.json")
-    os.rename("temp_json", "games.json")
+    write_json_game_db(json_list)
     
 
 def get_page(page):

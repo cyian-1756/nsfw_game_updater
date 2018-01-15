22ab266785362be4c07440adc3279268f3a835c2
cyian-1756 <devnull64@vfemail.net>
2018-01-15 00:10:14 -0500

    Added script to handle adding new games to the db

---
+++ add_new_game.py
@@ -0,0 +1,26 @@
+import pyfuncs
+import argparse
+
+parser = argparse.ArgumentParser(description="Keep track of your NSFW games")
+parser.add_argument("--game", help="Game name")
+parser.add_argument("--public-build", help="Link to the pages public builds")
+parser.add_argument("--setting", help="The setting the game takes place in")
+parser.add_argument("--developer", help="The dev")
+parser.add_argument("--engine", help="The engine the game uses")
+parser.add_argument("--visual-style", help="The visual style of the game")
+parser.add_argument("--genre", help="The game genre")
+parser.add_argument("--has-animation", help="Does the game have animations", action="store_true")
+parser.add_argument("--latest-version", help="The latest version of the game")
+parser.add_argument("--download-link-windows", help="The windows download link", default="Doesn't support")
+parser.add_argument("--download-link-linux", help="The Linux download link", default="Doesn't support")
+parser.add_argument("--download-link-mac", help="The Mac download link", default="Doesn't support")
+parser.add_argument("--download-link-android", help="The android download link", default="Doesn't support")
+args = parser.parse_args()
+
+json_to_add = {"game": args.game, "public_build": args.public_build, "setting":args.setting, \
+"developer": args.developer, "engine": args.engine, "visual_style":args.visual_style, "genre":args.genre, \
+"animation":args.has_animation, "latest_version":args.latest_version, "download_link_windows": args.download_link_windows, \
+"download_link_mac": args.download_link_mac, "download_link_linux": args.download_link_linux, \
+"download_link_android":args.download_link_android}
+
+pyfuncs.add_new_game(json_to_add)

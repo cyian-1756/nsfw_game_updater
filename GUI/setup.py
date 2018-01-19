f6bfa7fa2c45fa141da5a9495843fccda79442ae
Dogeek <Dogeek@users.noreply.github.com>
2018-01-19 23:20:11 +0100

    improved setup.py with included libraries. added virtualenv to gitignore

--- GUI/setup.py
+++ GUI/setup.py
@@ -6,7 +6,8 @@ import os
 os.environ['TCL_LIBRARY'] = r'D:\Softwares\python-362-x86\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'D:\Softwares\python-362-x86\tcl\tk8.6'
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "include_files":["help.html", "readme.md", "config.cfg", "games.json"]}
+build_exe_options = {"packages": ["os", "sys", "tkinter", "time", "configparser", "json", "requests", "re", "platform", "mediafire", "pyperclip", "webbrowser", "praw", "threading"]\
+					, "include_files":["help.html", "readme.md", "config.cfg", "games.json"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).

85293dd9599d6720c028e9d216d55545d5a0b3bd
Dogeek <Dogeek@users.noreply.github.com>
2018-01-28 02:50:29 +0100

    cleaned up

--- GUI/deprecated/setup-py2exe.py
+++
@@ -1,7 +0,0 @@
-# setup.py
-from distutils.core import setup
-import py2exe
-
-py2exeoptions = {"py2exe":{"optimize": 2, "bundle_files":1}}
-
-setup(windows=["gui.py"], options={"py2exe":{"optimize": 2, "bundle_files":1}})
